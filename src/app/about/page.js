70caaddbe860f52f72ea323f72e23e339ba5a528
ibsharakrami <91590546+ibsharakrami@users.noreply.github.com>
2024-02-24 19:54:29 +0530

    jamz about screen

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -5,11 +5,24 @@ export default function About() {
     <main className="h-screen  w-full rounded-md  items-center justify-center relative overflow-hidden mx-auto  md:py-0  md:flex ">
 
 
-      <div className="  h-1/2 w-full md:h-full flex items-center justify-center">
-          <h1 className="text-[10px]  md:text-[17px] font-bold text-left bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50  md:p-16 mt-6 p-4">The Modern Jamz Academy stands as a distinguished online institution devoted to enriching lives through the profound exploration of the Quran and the Arabic language. With a diverse array of meticulously crafted courses, we invite you to embark on a transformative journey of knowledge, spirituality, and personal growth. Our academy is more than an educational hub; it's a nurturing space where qualified instructors guide students in a supportive and interactive environment. Join us at [Your Academy Name] to elevate your understanding of the Quran, master the Arabic language, and deepen your connection to the teachings of Islam. Add a personal touch to your About section by sharing success stories from students who have benefited from our courses, creating a sense of community and inspiration for potential learners.</h1>
+      <div className="  h-1/2 w-full md:h-full  items-center justify-center">
+      <h1 className="text-2xl md:text-7xl font-bold text-left bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50 px-5 pt-12">
+      Unlock Quranic Wisdom with Us.
+        </h1>
+          <h1 className="text-[10px]  md:text-[17px] font-bold text-left bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50  md:p-16  p-5">The Modern Jamz Academy stands as a distinguished online institution devoted to enriching lives through the profound exploration of the Quran and the Arabic language. With a diverse array of meticulously crafted courses, we invite you to embark on a transformative journey of knowledge, spirituality, and personal growth. Our academy is more than an educational hub; it's a nurturing space where qualified instructors guide students in a supportive and interactive environment. Join us at [Your Academy Name] to elevate your understanding of the Quran, master the Arabic language, and deepen your connection to the teachings of Islam. Add a personal touch to your About section by sharing success stories from students who have benefited from our courses, creating a sense of community and inspiration for potential learners.</h1>
       </div>
-      <div className="bg-green-500  h-1/2 w-full items-center justify-center">
-      <h1>about </h1>
+      <div className="bg-green-500  h-1/2 w-full items-center justify-center flex gap-8">
+         <div className="bg-blue-300 h-60 w-60">
+
+         </div>
+         <div className="bg-red-300 gap-4 flex-col ">
+         <div className="bg-yellow-300 h-60 w-60">
+
+</div>
+  <div className="bg-green-300 h-60 w-60 mt-10">
+
+</div>
+</div>
       </div>
 
 
